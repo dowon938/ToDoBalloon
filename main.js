2a77c6d8af07c82f2bcbda2d22b976c43ca3ecf4
dowon938 <68101878+dowon938@users.noreply.github.com>
2021-01-23 00:05:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,26 +14,26 @@ textInput.addEventListener("keydown", (event) => {
   if (!eventKey) {
     return;
   }
-  eventKey.id=("p");
+  eventKey.id = "p";
 });
 textInput.addEventListener("keyup", (event) => {
   let eventKey = document.querySelector(`[data-key='${event.code}']`);
   if (!eventKey) {
     return;
   }
-  eventKey.id=("");
+  eventKey.id = "";
 });
-textInput.addEventListener('focus',()=>{
+textInput.addEventListener("focus", () => {
   keyboard.style.opacity = "1";
   typingContainer.style.transform = "translateY(0%)";
   textInput.style.width = "96%";
-  textInput.style.borderRadius = "15px"
-  textInput.style.transform = "translate(-50%, +34px)"
-})
-textInput.addEventListener('blur',()=>{
+  textInput.style.borderRadius = "15px";
+  textInput.style.transform = "translate(-50%, +34px)";
+});
+textInput.addEventListener("blur", () => {
   keyboard.style.opacity = "0";
   typingContainer.style.transform = "translateY(100%)";
   textInput.style.width = "50%";
-  textInput.style.borderRadius = "25px"
-  textInput.style.transform = "translate(-50%)"
-})
+  textInput.style.borderRadius = "25px";
+  textInput.style.transform = "translate(-50%)";
+});
